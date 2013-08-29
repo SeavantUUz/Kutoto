f5f96a84b27bc37260e8167e2d2278fcd379145d
AprocySanae <axdiaoqi220@gmail.com>
2013-08-29 22:43:22 +0000

    add index.html

---
+++ kutotike/forum/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+from forum.models import Parents_Tag,Tags,Topic,Post
+
+admin.site.register(Parents_Tag)
+admin.site.register(Tags)
+admin.site.register(Topic)
+admin.site.register(Post)
